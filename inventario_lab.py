f92bbe256b980f966822349d1c09b89617eccb80
Alexia Fuly <alexiafuly@eq.ufrj.br>
2026-07-26 04:11:36 +0000

    Adicionando comentários ao codigo

--- inventario_lab.py
+++ inventario_lab.py
@@ -21,23 +21,38 @@ purezas = [99.5, 92.0, 99.5, 98.0, 99.9, 98.5, 96.0, 99.0, 99.0, 98.8, 99.5, 92.
 
 # Identificação dos Tipos de Reagentes
 
+## Utilizando a função set para remover repetições na lista de reagentes
+
 set_reagentes = set(reagentes)
+
+## Imprimindo os reagentes disponíveis e a quantidade total de reagentes sem repetições
+
 print(f'\n Reagentes disponíveis: {set_reagentes}')
 print(f'\n Quantidade total de reagentes diferentes: {len(set_reagentes)}')
 
 # Estruturação do Inventário
 
+## Utilizando a função zip para combinar as três listas, gerando tuplas com um item de cada lista
+
 listas_combinadas = zip(reagentes, lotes, purezas)
+
+## Utilizando a função list para listar as tuplas
+
 lista_de_tuplas = list(listas_combinadas)
 
 # Geração de Relatório
 
 print('\n Relatório:')
+
+## Usando for para fazer o unpacking das tuplas, imprimindo um relatório com o frasco do lote, o nome do reagente e o grau de pureza
+
 for tupla in lista_de_tuplas:
    nome,lote,pureza = tupla
    print('Frasco do lote: {} | Reagente: {} | Pureza: {}%'.format(lote, nome, pureza))
 
 # Filtragem por Critério de Qualidade
+
+## Utilizando list comprehension para criar uma nova lista contendo somente os lotes que atendem ao critério de pureza
 
 lista_lotes_aprovados = [lote for nome, lote, pureza in lista_de_tuplas if pureza >= 98.0]
 print(f'\n Lotes aprovados com alto grau de pureza (>=98%): {lista_lotes_aprovados}')
